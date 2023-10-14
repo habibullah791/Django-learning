c0479c66481c8bd6981e169c9906436b60f43ed7
habibullah791 <habibullahshahid001@gmail.com>
2023-10-14 13:29:37 -0700

    Add Docstring in app

--- Chapter -2/django_project/pages/urls.py
+++ Chapter -2/django_project/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import homePageView
 
+# Define URL patterns for your Django app.
 urlpatterns = [
-    path("", homePageView, name="home")
+    path("", homePageView, name="home"),
 ]
+
--- Chapter -2/django_project/pages/views.py
+++ Chapter -2/django_project/pages/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
-
 def homePageView(request):
+    """
+        View function for the home page.
+
+        @desc:    This function takes a request object and returns an HttpResponse
+                    containing an HTML heading.
+
+        @params:  - request (HttpRequest): The request object sent by the client.
+
+        @returns:   HttpResponse: An HTTP response containing an HTML heading.
+
+        Example usage:
+        In your Django URL configuration, you can map a URL to this view function
+        to display the HTML heading on the home page.
+    """
     return HttpResponse("<h1>My First Heading</h1>")
